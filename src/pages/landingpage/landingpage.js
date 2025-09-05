9af059b8e1d6a9fa38855d621c39fe5619e82da1
BravoCoders <bravocodersunited@gmail.com>
2025-09-05 21:59:10 +0530

    feat: added responsiveness in herosection

--- src/pages/landingpage/landingpage.js
+++ src/pages/landingpage/landingpage.js
@@ -19,7 +19,12 @@ return(
           KRISHI MITRA</div>
         <p>"Krishi-Mitra is a smart farming app designed to support farmers with the power of AI and digital technology. From accurate weather forecasts and AI-based plant disease detection to local-language chatbots and easy video workshops, Krishi-Mitra makes modern farming simple, accessible, and effective. Whether you are looking to protect your crops, learn new methods, or stay updated with the latest agricultural insights, Krishi-Mitra is your trusted companion in the field."</p>
         <a href="#home">GET STARTED</a>
-        </div></section>
+        </div>
+        
+      
+      
+    
+  </section>
   );
 }
 export default LandingPage;
